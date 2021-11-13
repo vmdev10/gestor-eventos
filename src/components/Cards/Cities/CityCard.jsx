938d1425d8e79cea145e9c8f1ba1069f6e32187c
vmdev10 <mouraanav@gmail.com>
2021-11-13 00:56:07 -0300

    Created and styled CityCard component

---
+++ src/components/Cards/Cities/CityCard.jsx
@@ -0,0 +1,17 @@
+import './CityCard.css'
+
+import React from "react";
+
+function CityCard(props) {
+  return (
+    <div className="CityImage">
+      <img src={`/images/${props.imageUrl}`} alt="" />
+    
+      <div className="City">
+          <span>{ props.city }</span>
+      </div>
+    </div>
+  )
+}
+
+export default CityCard
