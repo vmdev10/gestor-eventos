86a1769fdd356d68766b4933330ae0d1f2b48a1d
vmdev10 <mouraanav@gmail.com>
2021-11-13 18:05:00 -0300

    Created and styled Categories component

---
+++ src/Pages/Categories.jsx
@@ -0,0 +1,29 @@
+import './Categories.css'
+
+import CategoryCard from '../components/Cards/Activities/ActivityCard'
+
+import React from "react";
+
+function Categories() {
+  return (
+    <div className="CategoriesComponent">
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+      <CategoryCard imageUrl="banner-gradient.jpg" category="Workshops"/>
+    </div>
+  )
+}
+
+export default Categories
