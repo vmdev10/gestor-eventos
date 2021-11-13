8d6cdd2bb0a9f877ec6b97e9045a8b77513025ae
vmdev10 <mouraanav@gmail.com>
2021-11-13 19:32:35 -0300

    Created 'Categories/:id' route and created and styled the Category component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Home from '../Pages/Home'
 import Navbar from '../components/Navbar/Navbar'
 import Categories from '../Pages/Categories';
+import Category from '../components/Category/Category'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from "react";
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/categories" element={<Categories />} />
+          <Route path="/categories/:id" element={<Category />}></Route>
         </Routes>
      </Router>
     </div>
---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,41 @@
+import './Category.css'
+
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+
+import React from "react";
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
+
+function Category() {
+  return (
+    <div className="CategoryContainer">
+      <div className="Category">
+        <Breadcrumbs className="Breadcumbs" aria-label="breadcrumb">
+          <Link color="inherit" href="/" onClick={handleClick}>
+            Home
+          </Link>
+          <Link
+            color="inherit"
+            href="/getting-started/installation/"
+            onClick={handleClick}
+          >
+            Categorias
+          </Link>
+          <Typography color="textPrimary">Categoria</Typography>
+        </Breadcrumbs>
+
+        <div className="CategoryPresentation">
+          <h3>Categoria</h3>
+          <p>Seja para se capacitar profissionalmente ou pelo simples prazer de aprender algo novo: escolha um curso ou workshop e fortaleça seu currículo!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
