8bfb656440e5aedc823ed6dd96c88e3ce236323c
vmdev10 <mouraanav@gmail.com>
2021-11-14 10:49:45 -0300

    Created Speakers section in Event

--- src/Pages/Event/Event.jsx
+++ src/Pages/Event/Event.jsx
@@ -45,12 +45,71 @@ function Event() {
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            remaining essentially unchanged. 
           </p>
         </div>
+      </div>
+
+      <div className="Speakers">
+        <h3>Palestrantes</h3>
+
+        <div className="AboutSpeakers">
+          <div>
+            <img src="/images/banner-gradient.jpg" alt="" />
+          </div>
+
+          <div className="AboutInformations">
+            <h1>Alice Mendes</h1>
+            <span>Analista de dados</span>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </p>
+          </div>
+        </div>
+
+        <div className="AboutSpeakers">
+        <div>
+          <img src="/images/banner-gradient.jpg" alt="" />
+        </div>
+
+        <div className="AboutInformations">
+          <h1>Alice Mendes</h1>
+          <span>Analista de dados</span>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+
+      </div>
+
+      <div className="AboutSpeakers">
+        <div>
+          <img src="/images/banner-gradient.jpg" alt="" />
+        </div>
+
+        <div className="AboutInformations">
+          <h1>Alice Mendes</h1>
+          <span>Analista de dados</span>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+      </div>
       </div>
     </>
   );
