2000616a393530aaa2975522902079b8c01fe85d
vmdev10 <mouraanav@gmail.com>
2021-11-14 12:42:20 -0300

    Speakers/Index.jsx component created

---
+++ src/components/Speakers/Index.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+function Speakers() {
+  return (
+    <div className="SpeakersContainer">
+      <h1>Palestrantes</h1>
+
+      <div className="AboutSpeakers">
+        <div>
+          <img src="/images/banner-gradient.jpg" alt="" />
+        </div>
+
+        <div className="AboutInformations">
+          <h1>Alice Mendes</h1>
+          <span>Analista de dados</span>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+      </div>
+
+      <div className="AboutSpeakers">
+        <div>
+          <img src="/images/banner-gradient.jpg" alt="" />
+        </div>
+
+        <div className="AboutInformations">
+          <h1>Alice Mendes</h1>
+          <span>Analista de dados</span>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+      </div>
+
+      <div className="AboutSpeakers">
+        <div>
+          <img src="/images/banner-gradient.jpg" alt="" />
+        </div>
+
+        <div className="AboutInformations">
+          <h1>Alice Mendes</h1>
+          <span>Analista de dados</span>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Speakers
