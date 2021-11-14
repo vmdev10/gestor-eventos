5c9f77127847825061f5bbe527c6aeb18eff719a
vmdev10 <mouraanav@gmail.com>
2021-11-14 13:48:45 -0300

    Definido informações de componente About via props

--- src/Pages/Event/Event.jsx
+++ src/Pages/Event/Event.jsx
@@ -3,7 +3,7 @@ import "./Event.css";
 import BannerEvent from "../../components/BannerEvent/Index";
 import About from "../../components/About/Index";
 import Speakers from "../../components/Speakers";
-import Programation from "../../components/Programation/Index";
+import Programation from "../../components/Programation";
 
 import React from "react";
 
@@ -12,10 +12,17 @@ function Event() {
     <>
       <BannerEvent />
 
-      <About />
+      <About
+        title="Quem somos"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+      />
       <Speakers />
       <Programation />
-      <About />
+      <About
+        title="O Local"
+        address="Av. Bernardino de Campos, 98 São Paulo - SP - CEP: 12345-678"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+      />
     </>
   );
 }
