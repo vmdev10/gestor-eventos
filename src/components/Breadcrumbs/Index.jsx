26805df26b0008a5c2e571213b55d771b420f9a1
vmdev10 <mouraanav@gmail.com>
2021-11-14 12:27:49 -0300

    I added the style to the Breadcrumbs component

--- src/components/Breadcrumbs/Index.jsx
+++ src/components/Breadcrumbs/Index.jsx
@@ -1,3 +1,5 @@
+import './Index.css'
+
 import Typography from "@material-ui/core/Typography";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import Link from "@material-ui/core/Link";
@@ -11,20 +13,22 @@ function handleClick(event) {
 
 function Breadcrumb() {
   return (
-    <Breadcrumbs className="Breadcumbs" aria-label="breadcrumb">
-      <Link color="inherit" href="/" onClick={handleClick}>
-        Home
-      </Link>
-      <Link
-        color="inherit"
-        href="/getting-started/installation/"
-        onClick={handleClick}
-      >
-        Categorias
-      </Link>
-      <Typography color="textPrimary">Categoria</Typography>
-    </Breadcrumbs>
+    <div className="BreadcrumbContainer">
+      <Breadcrumbs className="Breadcrumbs" aria-label="breadcrumb">
+        <Link color="inherit" href="/" onClick={handleClick}>
+          Home
+        </Link>
+        <Link
+          color="inherit"
+          href="/getting-started/installation/"
+          onClick={handleClick}
+        >
+          Categorias
+        </Link>
+        <Typography color="textPrimary">Categoria</Typography>
+      </Breadcrumbs>
+    </div>
   );
 }
 
-export default Breadcrumb
+export default Breadcrumb;
