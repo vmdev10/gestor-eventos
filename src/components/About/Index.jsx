7e8a21af49798a7d6476ee90e827a71022da0e4c
vmdev10 <mouraanav@gmail.com>
2021-11-14 12:37:33 -0300

    About/Index.jsx component created

---
+++ src/components/About/Index.jsx
@@ -0,0 +1,28 @@
+import './Index.css'
+
+
+import React from "react";
+
+function About() {
+  return (
+    <div className="AboutContainer">
+      <div>
+        <img src="/images/banner-gradient.jpg" alt="" />
+      </div>
+
+      <div className="AboutInformations">
+        <h1>Quem Somos</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About
