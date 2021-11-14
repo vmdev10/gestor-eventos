58c5b579133b06a19d5894d4daf8c3c1d3a20bf2
vmdev10 <mouraanav@gmail.com>
2021-11-14 13:11:25 -0300

    Programation/Index.jsx component created

---
+++ src/components/Programation/Index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Programation() {
+  return (
+    <div className="ProgramationContainer">
+      <h1>Programação</h1>
+
+      <div className="ProgramationContainer">
+        <div>
+          <h1>
+            <strong>9:00</strong>
+          </h1>
+          <span className="Speaker">VINÍCUIS MARTINS:</span>
+          <span>Conteúdo é o Rei. Design é a Rainha</span>
+        </div>
+        <div>
+          <h1>
+            <strong>9:00</strong>
+          </h1>
+          <span className="Speaker">VINÍCUIS MARTINS:</span>
+          <span>Conteúdo é o Rei. Design é a Rainha</span>
+        </div>
+        <div>
+          <h1>
+            <strong>9:00</strong>
+          </h1>
+          <span className="Speaker">VINÍCUIS MARTINS:</span>
+          <span>Conteúdo é o Rei. Design é a Rainha</span>
+        </div>
+        <div>
+          <h1>
+            <strong>9:00</strong>
+          </h1>
+          <span className="Speaker">VINÍCUIS MARTINS:</span>
+          <span>Conteúdo é o Rei. Design é a Rainha</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Programation
