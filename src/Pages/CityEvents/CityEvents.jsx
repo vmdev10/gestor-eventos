7a41c0e1deb1b3fdb21a0267a2e64ae0f30e4524
vmdev10 <mouraanav@gmail.com>
2021-11-13 23:09:25 -0300

    Created and styled CityEvents component and created city route

---
+++ src/Pages/CityEvents/CityEvents.jsx
@@ -0,0 +1,13 @@
+import CityEvents from '../Category/Category'
+
+import React from "react";
+
+function City() {
+  return (
+    <div className="CityEventsContainer">
+      <CityEvents />
+    </div>
+  )
+}
+
+export default City
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Home from '../Pages/Home'
 import Navbar from '../components/Navbar/Navbar'
 import Categories from '../Pages/Categories';
 import Category from '../Pages/Category/Category'
+import City from '../Pages/CityEvents/CityEvents'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from "react";
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/categories/:id" element={<Category />}></Route>
+          <Route path="/city" element={<City />}></Route>
         </Routes>
      </Router>
     </div>
