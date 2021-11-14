96cd937d57baffb660ce38f3961f7efcaf35c060
vmdev10 <mouraanav@gmail.com>
2021-11-14 11:58:50 -0300

    Created and styled the Local section

--- src/Pages/Event/Event.jsx
+++ src/Pages/Event/Event.jsx
@@ -112,7 +112,7 @@ function Event() {
       </div>
 
       <div className="Programation">
-          <h1>Programação</h1>
+        <h1>Programação</h1>
 
         <div className="ProgramationContainer">
           <div>
@@ -143,6 +143,27 @@ function Event() {
             <span className="Speaker">VINÍCUIS MARTINS:</span>
             <span>Conteúdo é o Rei. Design é a Rainha</span>
           </div>
+        </div>
+      </div>
+
+      <div className="Location">
+        <div>
+          <img src="/images/banner-gradient.jpg" alt="" />
+        </div>
+
+        <div className="LocationAddress">
+          <h1>O Local</h1>
+
+          <span>
+            Av. Bernardino de Campos, 98 São Paulo - SP - CEP: 12345-678
+          </span>
+
+          <p>
+            SOBRE O LOCAL Sou um parágrafo. Clique aqui para editar e adicionar
+            o seu próprio texto. É fácil! Basta clicar em "Editar Texto" ou
+            clicar duas vezes sobre mim e você poderá adicionar o seu próprio
+            conteúdo e trocar fontes.
+          </p>
         </div>
       </div>
     </>
