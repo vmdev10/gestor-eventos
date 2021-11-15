80185d9958b4e1ecdc5fc7d5dbf40531cfe6d55c
vmdev10 <mouraanav@gmail.com>
2021-11-15 16:04:57 -0300

    Treating the number of cards by Boxes in CarouselDesktop

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,4 +1,4 @@
-  import "./Events.css";
+import "./Events.css";
 
 import Carousel from "../Carousel/Carousel";
 import EventCard from "../Cards/Event/EventCard";
@@ -12,67 +12,67 @@ const eventsData = [
     id: 1,
     lat: 0,
     lng: 0,
-    name: 'Some name',
-    description: 'Some Description',
-    address: 'Santa Helena, 370, Picos, Piauí, Brasil',
-    eventDate: 'QUA, 10 NOV - 17:00',
+    name: "Some name",
+    description: "Some Description",
+    address: "Santa Helena, 370, Picos, Piauí, Brasil",
+    eventDate: "QUA, 10 NOV - 17:00",
     categorieId: 1,
-    imageUrl: 'banner-gradient.jpg'
+    imageUrl: "banner-gradient.jpg",
   },
   {
     id: 1,
     lat: 0,
     lng: 0,
-    name: 'Some name',
-    description: 'Some Description',
-    address: 'Santa Helena, 370, Picos, Piauí, Brasil',
-    eventDate: 'QUA, 10 NOV - 17:00',
+    name: "Some name",
+    description: "Some Description",
+    address: "Santa Helena, 370, Picos, Piauí, Brasil",
+    eventDate: "QUA, 10 NOV - 17:00",
     categorieId: 1,
-    imageUrl: 'banner-gradient.jpg'
+    imageUrl: "banner-gradient.jpg",
   },
   {
     id: 1,
     lat: 0,
     lng: 0,
-    name: 'Some name',
-    description: 'Some Description',
-    address: 'Santa Helena, 370, Picos, Piauí, Brasil',
-    eventDate: 'QUA, 10 NOV - 17:00',
+    name: "Some name",
+    description: "Some Description",
+    address: "Santa Helena, 370, Picos, Piauí, Brasil",
+    eventDate: "QUA, 10 NOV - 17:00",
     categorieId: 1,
-    imageUrl: 'banner-gradient.jpg'
+    imageUrl: "banner-gradient.jpg",
   },
   {
     id: 1,
     lat: 0,
     lng: 0,
-    name: 'Some name',
-    description: 'Some Description',
-    address: 'Santa Helena, 370, Picos, Piauí, Brasil',
-    eventDate: 'QUA, 10 NOV - 17:00',
+    name: "Some name",
+    description: "Some Description",
+    address: "Santa Helena, 370, Picos, Piauí, Brasil",
+    eventDate: "QUA, 10 NOV - 17:00",
     categorieId: 1,
-    imageUrl: 'banner-gradient.jpg'
+    imageUrl: "banner-gradient.jpg",
   },
   {
     id: 1,
     lat: 0,
     lng: 0,
-    name: 'Some name',
-    description: 'Some Description',
-    address: 'Santa Helena, 370, Picos, Piauí, Brasil',
-    eventDate: 'QUA, 10 NOV - 17:00',
+    name: "Some name",
+    description: "Some Description",
+    address: "Santa Helena, 370, Picos, Piauí, Brasil",
+    eventDate: "QUA, 10 NOV - 17:00",
     categorieId: 1,
-    imageUrl: 'banner-gradient.jpg'
+    imageUrl: "banner-gradient.jpg",
   },
   {
     id: 1,
     lat: 0,
     lng: 0,
-    name: 'Some name',
-    description: 'Some Description',
-    address: 'Santa Helena, 370, Picos, Piauí, Brasil',
-    eventDate: 'QUA, 10 NOV - 17:00',
+    name: "Some name",
+    description: "Some Description",
+    address: "Santa Helena, 370, Picos, Piauí, Brasil",
+    eventDate: "QUA, 10 NOV - 17:00",
     categorieId: 1,
-    imageUrl: 'banner-gradient.jpg'
+    imageUrl: "banner-gradient.jpg",
   },
 ];
 
@@ -81,30 +81,32 @@ function Events(props) {
 
   useEffect(() => {
     setEvents(eventsData);
-  }, [])
+  }, []);
 
   const handleFormatBoxes = (allElements, quantityElementsPerBox) => {
     const boxes = [];
 
     const quantityBox = allElements.length / quantityElementsPerBox;
-    
-    for (let index = 0; index < quantityBox; index++) {
 
+    for (let index = 0; index < quantityBox; index++) {
       const initialElementIndex = index * quantityElementsPerBox;
-      const endElementIndex = initialElementIndex + quantityElementsPerBox
-      const currenElements = allElements.slice(initialElementIndex, endElementIndex);
+      const endElementIndex = initialElementIndex + quantityElementsPerBox;
+      const currenElements = allElements.slice(
+        initialElementIndex,
+        endElementIndex
+      );
 
       boxes.push({
-        currenElements
-      })
+        currenElements,
+      });
     }
 
-    return boxes;    
-  }
+    return boxes;
+  };
 
   return (
     <div className="EventsContainer">
-      <h3> { props.event }</h3>
+      <h3> {props.event}</h3>
 
       <div className="CarouselContainer">
         <a href="">
@@ -113,38 +115,39 @@ function Events(props) {
         {/* {
           console.log(handleFormatBoxes(events, 4))
         } */}
-       
-       <div className="CarouselMobile">
-        <Carousel>
-            {
-              handleFormatBoxes(events, 2).map((box) => (
-                <Box className="CarouselCardsContainer">
-                  {
-                    box.currenElements.map((eventCard) => (
-                      <EventCard imageUrl={eventCard.imageUrl} eventDate={eventCard.eventDate} name={eventCard.name} address={eventCard.address}/>
-                    ))
-                  }
-                </Box>
-              ))
-            }
-        </Carousel>
-       </div>
-       <div className="CarouselDesktop">
-        <Carousel>
-            <Box className="CarouselCardsContainer">
-              <EventCard imageUrl='banner-gradient.jpg'/>
-              <EventCard imageUrl='banner-gradient.jpg'/>
-              <EventCard imageUrl='banner-gradient.jpg'/>
-              <EventCard imageUrl='banner-gradient.jpg'/>
-            </Box>
-            <Box className="CarouselCardsContainer">
-              <EventCard imageUrl='banner-gradient.jpg'/>
-              <EventCard imageUrl='banner-gradient.jpg'/>
-              <EventCard imageUrl='banner-gradient.jpg'/>
-              <EventCard imageUrl='banner-gradient.jpg'/>
-            </Box>
+
+        <div className="CarouselMobile">
+          <Carousel>
+            {handleFormatBoxes(events, 2).map((box) => (
+              <Box className="CarouselCardsContainer">
+                {box.currenElements.map((eventCard) => (
+                  <EventCard
+                    imageUrl={eventCard.imageUrl}
+                    eventDate={eventCard.eventDate}
+                    name={eventCard.name}
+                    address={eventCard.address}
+                  />
+                ))}
+              </Box>
+            ))}
           </Carousel>
-       </div>
+        </div>
+        <div className="CarouselDesktop">
+          <Carousel>
+            {handleFormatBoxes(events, 4).map((box) => (
+              <Box className="CarouselCardsContainer">
+                {box.currenElements.map((eventCard) => (
+                  <EventCard
+                    imageUrl={eventCard.imageUrl}
+                    eventDate={eventCard.eventDate}
+                    name={eventCard.name}
+                    address={eventCard.address}
+                  />
+                ))}
+              </Box>
+            ))}
+          </Carousel>
+        </div>
       </div>
     </div>
   );
