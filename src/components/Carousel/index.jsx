fea19e8fb746b065f9bcfc0d37e44de9b362a09d
vmdev10 <mouraanav@gmail.com>
2021-11-16 16:05:38 -0300

    Improved style of alignment in BannerEvent

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -10,7 +10,7 @@ function Carousel(props) {
   return (
     <CarouselMUI
       className="Carousel"
-      autoPlay={props.autoPlay || true }
+      autoPlay={props.autoPlay || false }
       interval={props.interval || 3000 }
       animation={props.animation || 'slide'}
       navButtonsAlwaysVisible={ props.navButtonsAlwaysVisible || false }
